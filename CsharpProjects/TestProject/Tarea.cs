62ac53cda8ab10cdb334ba1ed4288e1cb2239053
Alexielt <angie.adame.lopez@gmail.com>
2026-09-15 19:58:00 -0600

    COMMIT_LISTA_TAREAS

--- CsharpProjects/TestProject/Tarea.cs
+++ CsharpProjects/TestProject/Tarea.cs
@@ -2,5 +2,15 @@ public class Tarea
 {
     public string Titulo { get; set; } = "";
     public string Materia { get; set; } = "";
+    public string Descripcion { get; set; } = "";
     public DateTime FechaEntrega { get; set; }
+    public string Estado { get; set; } = "PENDIENTE";
+
+public Tarea(string titulo, string materia, string descripcion, DateTime fechaEntrega)
+{
+    Titulo = titulo;
+    Materia = materia;
+    Descripcion = descripcion;
+    FechaEntrega = fechaEntrega;
+} 
 }
